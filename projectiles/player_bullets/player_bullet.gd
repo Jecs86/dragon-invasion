e07504fed2c40a86bf441d4b35ec354590c17992
Jecs86 <jecarrer@espol.edu.ec>
2026-06-21 23:47:34 -0500

    feat: Add bullet for the player

---
+++ projectiles/player_bullets/player_bullet.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export var velocity: float = 600.0
+var direction: Vector2 = Vector2.RIGHT
+
+func _ready() -> void:
+	$VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
+
+func _process(delta: float) -> void:
+	global_position += direction * velocity * delta
